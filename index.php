420300ada785201b14163136d66a290eb21524a4
Fazal Said <ichaldotid@gmail.com>
2018-08-01 16:13:50 +0700

    Add files to upload

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html>
+<head>
+<title>Daftar Harga Web Builder FPWare</title>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Slide Pricing Tables Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<link href="//fonts.googleapis.com/css?family=Roboto+Slab:100,400,700" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script src="js/jquery-2.1.4.min.js"></script>
+</head>
+<body>
+<div class="content" id="Main-Content">
+    <center><img src="images/crudion.png" width="170" height="90" /></center>
+	<h1>Daftar Harga Web Builder FPWare</h1>
+	<div class="agileits-top-slider">
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides callbacks callbacks1" id="slider4">
+					<li>
+						<div class="pricing">
+							<div class="pricing-top green-top">
+								<h3>Si Kecil</h3>
+								<p>Rp<span>400.000</span>/Web</p>
+							</div>
+							<div class="pricing-bottom">
+								<div class="pricing-bottom-top">
+									<p>Dengan harga 400.000, Anda sudah bisa mendapatkan website impian Anda.</p>
+								</div>
+								<div class="pricing-bottom-bottom">
+									<p><span>Fitur</span> Terbatas</p>
+									<p><span>Penyimpanan </span> 500MB</p>  
+									<p><span>2</span> Subdomains</p>
+								</div>
+								<div class="buy-button">
+									<a class="popup-with-zoom-anim" href="#small-dialog">Lihat Selengkapnya.</a>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="pricing">
+							<div class="pricing-top blue-top">
+								<h3>Remaja</h3>
+								<p>Rp<span>800.000</span>/Web</p>
+							</div>
+							<div class="pricing-bottom">
+								<div class="pricing-bottom-top">
+									<p>Anda bisa mendapatkan website dengan fitur lebih banyak!</p>
+								</div>
+								<div class="pricing-bottom-bottom">
+									<p><span>Fitur</span> Lebih Banyak</p>
+									<p><span>Penyimpanan </span> 1GB</p>  
+									<p><span>8</span> Subdomains</p>
+								</div>
+								<div class="buy-button">
+									<a class="popup-with-zoom-anim" href="#small-dialog">Lihat Selengkapnya</a>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="pricing">
+							<div class="pricing-top">
+								<h3>Dewasa</h3>
+								<p>Rp<span>4.000.000</span>/Web</p>
+							</div>
+							<div class="pricing-bottom">
+								<div class="pricing-bottom-top">
+									<p>Hanya untuk Anda para Pebisnis Profesional, dapatkan UNLIMITED FEATURES sekarang juga!</p>
+								</div>
+								<div class="pricing-bottom-bottom">
+									<p><span>Fitur</span> Tak Terbatas</p>
+									<p><span>Penyimpanan </span> 3GB</p>  
+									<p><span>15</span> Subdomains</p>
+								</div>
+								<div class="buy-button">
+									<a class="popup-with-zoom-anim" href="#small-dialog">Lihat Selengkapnya</a>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clear"> </div>
+			<script src="js/responsiveslides.min.js"></script>
+			<script>
+				// You can also use "$(window).load(function() {"
+				$(function () {
+					// Slideshow 4
+					$("#slider4").responsiveSlides({
+					auto: true,
+					pager:true,
+					nav:true,
+					speed: 500,
+					namespace: "callbacks",
+					before: function () {
+							$('.events').append("<li>before event fired.</li>");
+						},
+						after: function () {
+							$('.events').append("<li>after event fired.</li>");
+						}
+					});
+					
+				});
+			 </script>
+			<!--banner Slider starts Here-->
+		</div>
+	</div>
+	<!--pop-up-grid-->
+	<div id="popup">
+		<div id="small-dialog" class="mfp-hide">
+			<div class="signin-form profile">
+				<h3>Tidak Ditemukan!</h3>
+				
+				<p>Mohon maaf, halaman yang Anda cari tidak ditemukan.</p>
+			</div>
+		</div>
+	</div>						
+	<!--pop-up-grid-->
+	<div class="copyright">
+		<p class="footer">© 2018 FPWare. All Rights Reserved.</p>
+	</div>
+	<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+	<script>
+		$(document).ready(function() {
+			$('.popup-with-zoom-anim').magnificPopup({
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				removalDelay: 300,
+				mainClass: 'my-mfp-zoom-in'
+		});
+																						
+	});
+	</script>
+</div>
+
+</body>
+</html>
